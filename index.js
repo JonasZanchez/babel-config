ee426f932d4aa50809e42ac6180f4e319d0edb29
luojiaxin <luojiaxin@yunquna.com>
2023-09-07 19:10:37 +0800

    创建一个自定义babel

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-module.exports = {
-    presets: [
-        require("@babel/preset-env"),
-        require("@babel/preset-typescript"),
-        require("@babel/preset-react")
-    ],
-    plugins: [
-        require("@babel/plugin-transform-typescript")
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = [
+        // require('@babel/preset-react'),
+        // require('@babel/preset-typescrip'),
+        // require('@babel/preset-env'),
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+        '@babel/preset-env'
+    ];
+    const plugins = [
+        [
+            '@babel/plugin-transform-typescript',
+            {
+                isTSX: true,
+                allowNamespaces: true,
+            },
+        ],
     ]
-}
+
+    return {
+        presets,
+        plugins
+    };
+};  
